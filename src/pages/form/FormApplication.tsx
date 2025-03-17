d45419257dc2f18d2a51f86a1bafdf0e60a20984
Serge <sergunkit@MacBookPro.HomeLAN>
2025-03-17 20:00:05 +0300

    add pretty view and session storage

--- src/pages/form/FormApplication.tsx
+++ src/pages/form/FormApplication.tsx
@@ -10,83 +10,73 @@ export function JobApplicationForm() {
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    // Обработка данных формы
-    console.log({
-      skill,
-      role,
-      hireDate,
-      stack,
-    });
+    
+    const formData = { skill, role, hireDate, stack };
+    sessionStorage.setItem('jobApplicationData', JSON.stringify(formData));
+    console.log('Данные формы:', formData);
+    
   };
 
   const navigate = useNavigate();
 
-  const handleNavigation = () => {
-    navigate('/');
-  };
-
   return (
-    <div className="form-application-details">
-      <h2>Форма подачи заявки на прием работника</h2>
-      <form onSubmit={handleSubmit} className="form-header">
-        <div>
-          <label htmlFor="stack">Выберите стек:  </label>
-          <select
-            id="stack"
-            value={stack}
-            onChange={(e) => setStack(e.target.value)}
-          >
-            <option value="">-- Выберите стек технологий --</option>
-            <option value="typescript">JavaScript</option>
-            <option value="python">Python</option>
-            <option value="java">Java</option>
-            <option value="go">C#</option>
-            <option value="c++">PHP</option>
-            {/* Добавьте другие языки программирования по мере необходимости */}
-          </select>
-        </div>
+    <div >
+        <h2 className="header">Форма подачи заявки на прием работника</h2>
+        <form className="form-application" onSubmit={handleSubmit}>
+            <label className="label1" htmlFor="stack">Выберите стек:</label>
+            <select
+                className="input1"
+                id="stack"
+                value={stack}
+                onChange={(e) => setStack(e.target.value)}
+            >
+                <option value="">-  Выберите язык программирования  -</option>
+                <option value="typescript">JavaScript</option>
+                <option value="python">Python</option>
+                <option value="java">Java</option>
+                <option value="go">C#</option>
+                <option value="c++">PHP</option>
+            </select>
+     
+            <label className="label2" htmlFor="skill">Введите навык:</label>
+            <select
+                className="input2"
+                id="skill"
+                value={skill}
+                onChange={(e) => setSkill(e.target.value)}
+            >
+                <option value="">-  Выберите уровень навыков  -</option>
+                <option value="typescript">junior</option>
+                <option value="python">middle</option>
+                <option value="java">senior</option>
+            </select>
+         
+            <label className="label3" htmlFor="role">Выберите должность:  </label>
+            <select
+                className="input3"
+                id="role"
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+            >
+                <option value="">-  Выберите должность  -</option>
+                <option value="frontend">Frontend Developer</option>
+                <option value="backend">Backend Developer</option>
+                <option value="fullstack">Fullstack Developer</option>
+            </select>
 
-        <div>
-          <label htmlFor="skill">Введите навык:</label>
-          <input
-            type="text"
-            id="skill"
-            value={skill}
-            onChange={(e) => setSkill(e.target.value)}
-          />
-        </div>
+            <label className="label4" htmlFor="hireDate">Дата выхода сотрудника:  </label>
+            <input
+                className="input4"
+                type="date"
+                id="hireDate"
+                value={hireDate}
+                onChange={(e) => setHireDate(e.target.value)}
+            />
 
-        <div>
-          <label htmlFor="role">Выберите должность:  </label>
-          <select
-            id="role"
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-          >
-            <option value="">-- Выберите должность --</option>
-            <option value="frontend">Frontend Developer</option>
-            <option value="backend">Backend Developer</option>
-            <option value="fullstack">Fullstack Developer</option>
-            {/* Добавьте другие должности по мере необходимости */}
-          </select>
-        </div>
-
-        <div>
-          <label htmlFor="hireDate">Дата выхода сотрудника:  </label>
-          <input
-            type="date"
-            id="hireDate"
-            value={hireDate}
-            onChange={(e) => setHireDate(e.target.value)}
-          />
-        </div>
-
-        <div className="form-button">
-          <button type="submit">Сохранить</button>
-          <button onClick={handleNavigation} type='button'>
-            Добавить
-          </button>
-        </div>
+            <button className="button1" type="submit">Добавить</button>
+            <button className="button2" onClick={() => navigate('/', { replace: false })} type='button'>
+                Сохранить
+            </button>
       </form>
     </div>
   );
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,12 +1,12 @@
-export { Applications } from './applications';
-export { Boundary } from './boundary';
-export { Employees } from './employees';
-export { Home } from './home';
-export { Profile } from './profile';
-export { Status } from './status';
-export { Structure } from './structure';
-export { TeamDetail } from './teams';
-export { TeamList } from './teams';
-export { Welcome } from './welcome';
+// export { Applications } from './applications';
+export { Boundary } from './boundary/Boundary.tsx';
+// export { Employees } from './employees';
+// export { Home } from './home';
+// export { Profile } from './profile';
+// export { Status } from './status';
+// export { Structure } from './structure';
+// export { TeamDetail } from './teams';
+// export { TeamList } from './teams';
+// export { Welcome } from './welcome';
 // export { FormApplication } from './teams/team-detail/FormApplication.tsx'
 export { JobApplicationForm } from './form/FormApplication.tsx'
